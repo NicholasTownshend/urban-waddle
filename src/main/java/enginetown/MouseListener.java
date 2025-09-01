d93b360525be795e10da7c3eb22bebb9e44befe2
towny3 <nicholas.townshend3@gmail.com>
2025-09-01 21:57:40 +1000

    fixed bug in MouseListener.java

--- src/main/java/enginetown/MouseListener.java
+++ src/main/java/enginetown/MouseListener.java
@@ -41,7 +41,7 @@ public class MouseListener {
             }
         } else if (action == GLFW_RELEASE) {
             if (button < get().mouseButtonPressed.length) {
-                get().mouseButtonPressed[button] = true;
+                get().mouseButtonPressed[button] = false;
                 get().isDragging = false;
             }
         }
