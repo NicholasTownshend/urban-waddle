41e77893faf6920f28fca1e399b644d8d76ba21d
towny3 <nicholas.townshend3@gmail.com>
2025-09-01 22:08:29 +1000

    added time keeping code to Window.loop

--- src/main/java/enginetown/Window.java
+++ src/main/java/enginetown/Window.java
@@ -3,6 +3,7 @@ package enginetown;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -88,6 +89,9 @@ public class Window {
     }
 
     public void loop() {
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+
         while (!glfwWindowShouldClose(glfwWindow)) {
             // Poll events
             glfwPollEvents();
@@ -96,6 +100,10 @@ public class Window {
             glClear(GL_COLOR_BUFFER_BIT);
 
             glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            float dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 }
